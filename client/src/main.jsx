7433edd659c81cba4b22ea8f36a530cbd7702c1e
waiyanphioe <waiyanphioe@gmail.com>
2023-06-03 17:10:37 +0630

    route error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { theme } from "./utils/theme.js";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductDetails from "./screens/productDetails/productDetails.jsx";
+import Home from "./screens/home/Home.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <div>Error</div>,
+	},
+	{
+		path: "products/:productId",
+		element: <ProductDetails />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<App />
-			</ThemeProvider>
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<RouterProvider router={router} />
+		</ThemeProvider>
 	</React.StrictMode>
 );
--- client/src/screens/itemDetails/itemDetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ItemDetails = () => {
-	return <div>ItemDetails</div>;
-};
-
-export default ItemDetails;
