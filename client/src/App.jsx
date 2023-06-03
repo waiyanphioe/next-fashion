a941b9a351a9d4af961240b83512e589a3e1cce9
waiyanphioe <waiyanphioe@gmail.com>
2023-06-03 17:01:04 +0630

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./screens/home/Home";
+import ItemDetails from "./screens/itemDetails/itemDetails";
 
-function App() {
-	return <div className=""></div>;
-}
+const ScrollToTop = () => {
+	const { pathname } = useLocation();
+
+	React.useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+	return null;
+};
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<ScrollToTop />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/item/:itemId" element={<ItemDetails />} />
+			</Routes>
+		</React.Fragment>
+	);
+};
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./utils/theme.js";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
