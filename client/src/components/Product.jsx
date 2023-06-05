0661272207c5e5219cdc87d7ba19b22dcbe56ca3
waiyanphioe <waiyanphioe@gmail.com>
2023-06-05 23:04:37 +0630

    change

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,13 +1,113 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { Box, IconButton, Typography, Button } from "@mui/material";
+import { Box, IconButton, Typography, Button, useTheme } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import { shades } from "../utils/theme";
 import { addToCart } from "../redux/cartSlice";
+import { useNavigate } from "react-router-dom";
 
-const Product = () => {
-	return <div>Product</div>;
+const Product = ({ item, width }) => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [count, setCount] = React.useState(1);
+	const [isHovered, setIsHovered] = React.useState(false);
+	const {
+		palette: { neutral },
+	} = useTheme();
+
+	// const { category, price, name, image } = item.attributes;
+
+	// const {
+	// 	data: {
+	// 		attributes: {
+	// 			formats: {
+	// 				medium: { url },
+	// 			},
+	// 		},
+	// 	},
+	// } = image;
+
+	return (
+		<Box width={width}>
+			<Box
+				sx={{ position: "relative" }}
+				onMouseOver={() => setIsHovered(true)}
+				onMouseOut={() => setIsHovered(false)}
+			>
+				<img
+					src={item.image}
+					alt={item.name}
+					width={"100%"}
+					height={"100%"}
+					onClick={() => navigate(`/product/${item.id}`)}
+					style={{ cursor: "pointer" }}
+				/>
+				<Box
+					display={isHovered ? "block" : "none"}
+					sx={{
+						position: "absolute",
+						bottom: "10%",
+						left: 0,
+						width: "100%",
+						padding: "0 5%",
+					}}
+				>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+						}}
+					>
+						<Box
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								backgroundColor: shades.neutral[100],
+								borderRadius: "3px",
+							}}
+						>
+							<IconButton
+								onClick={() => setCount(Math.max(count - 1, 1))}
+							>
+								<RemoveIcon />
+							</IconButton>
+							<Typography color={shades.primary[300]}>
+								{count}
+							</Typography>
+							<IconButton onClick={() => setCount(count + 1)}>
+								<AddIcon />
+							</IconButton>
+						</Box>
+
+						<Button
+							onClick={() =>
+								dispatch(
+									addToCart({ item: { ...item, count } })
+								)
+							}
+							sx={{
+								backgroundColor: shades.primary[300],
+								color: "white",
+							}}
+						>
+							Add to Cart
+						</Button>
+					</Box>
+				</Box>
+			</Box>
+
+			<Box mt={"3px"}>
+				<Typography variant="subtitle2" color={neutral.dark}>
+					{item.category
+						.replace(/([A-Z])/g, "$1")
+						.replace(/^./, (str) => str.toUpperCase())}
+				</Typography>
+				<Typography>{item.name}</Typography>
+				<Typography fontWeight={"bold"}>$ {item.price}</Typography>
+			</Box>
+		</Box>
+	);
 };
 
 export default Product;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,29 @@
 // import React from "react";
-import { Link } from "react-router-dom";
+import Product from "../components/Product";
+// import MainCarousel from "../components/MainCarousel";
+
+const data = {
+	id: 2,
+	name: "White Sweater TShirt",
+	shortDescription:
+		"Donec sollicitudin molestie malesuada. Vivamus suscipit.",
+	longDescription:
+		"Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.\n",
+	price: 15,
+	category: "topRated",
+	createdAt: "2022-10-17T17:17:21.884Z",
+	updatedAt: "2022-10-17T17:17:23.404Z",
+	publishedAt: "2022-10-17T17:17:23.402Z",
+	image: "https://assets.vogue.in/photos/5fe1eeca3d9c144d784175c7/2:3/w_2560%2Cc_limit/Fashion%2520Diva%2520Dhawan.jpg",
+	createdBy: null,
+	updatedBy: null,
+};
 
 const Home = () => {
 	return (
 		<div>
-			<Link to="products/pr">Go Product</Link>
+			{/* <MainCarousel /> */}
+			<Product item={data} width={"250px"} />
 		</div>
 	);
 };
